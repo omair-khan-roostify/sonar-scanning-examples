7b874058a3bcc8e791493238e81311c2e9c4de79
Omair <okhan@roostify.com>
2018-08-14 11:50:40 -0700

    new bugs

--- sonarqube-scanner-gradle/src/main/java/App.java
+++ sonarqube-scanner-gradle/src/main/java/App.java
@@ -16,6 +16,11 @@ public class App {
         System.out.println("Hello");
     }
 
+    public void test3(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
