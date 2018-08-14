14da44521b6651ac9f816027f0277276c8049cd9
Omair <okhan@roostify.com>
2018-08-14 11:46:37 -0700

    Adding a new bug

--- sonarqube-scanner-gradle/src/main/java/App.java
+++ sonarqube-scanner-gradle/src/main/java/App.java
@@ -5,6 +5,17 @@ public class App {
     public String getGreeting() {
         return "Hello world.";
     }
+    
+    public void test1(){
+	System.out.println("Hello");
+        System.out.println("Hello");
+    }
+
+    public void test2(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
