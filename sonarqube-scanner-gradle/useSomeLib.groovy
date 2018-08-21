bed2dbedb0ad3d884c7e2fd110e5ac468514cd7b
omair-khan-roostify <42222955+omair-khan-roostify@users.noreply.github.com>
2018-08-20 17:10:06 -0700

    Delete useSomeLib.groovy

--- sonarqube-scanner-gradle/useSomeLib.groovy
+++
@@ -1,8 +0,0 @@
-def useSomeLib(helper) { // dynamic: cannot declare as Helper
-    helper.prepare()
-    return helper.count()
-}
-
-def lib = library('my-shared-library').com.mycorp.pipeline // preselect the package
-
-echo useSomeLib(lib.Helper.new(lib.Constants.SOME_TEXT))
