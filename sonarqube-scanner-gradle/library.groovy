5500adea2b45e1f0cdf36968e1f58d8412b5a314
omair-khan-roostify <42222955+omair-khan-roostify@users.noreply.github.com>
2018-08-20 17:09:56 -0700

    Delete library.groovy

--- sonarqube-scanner-gradle/library.groovy
+++
@@ -1,9 +0,0 @@
-@Library('somelib')
-import http://localhost:8080/job/my-test/job/master/
-
-int useSomeLib(Helper helper) {
-    helper.prepare()
-    return helper.count()
-}
-
-echo useSomeLib(new Helper('some text'))
